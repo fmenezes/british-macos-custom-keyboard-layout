0e3005606742436c029ec3f10a0c28745d5585e7
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2021-02-12 16:51:42 +0000

    Remove automator

--- script/install.sh
+++ script/install.sh
@@ -3,5 +3,3 @@
 git clone https://github.com/fmenezes/british-macos-custom-keyboard-layout.git ~/.britishcustomkeyboard
 
 ln -s ~/.britishcustomkeyboard/British_Latin.bundle ~/Library/Keyboard\ Layouts/British_Latin.bundle
-
-automator ~/.britishcustomkeyboard/scripts/UpdateLanguage.workflow 2>&1 1>/dev/null
