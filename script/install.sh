91607e214c948a324203762d531b8db99e7bddbf
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2021-02-12 16:55:20 +0000

    Update bundle file name and version

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 git clone https://github.com/fmenezes/british-macos-custom-keyboard-layout.git ~/.britishcustomkeyboard
-
-ln -s ~/.britishcustomkeyboard/British_Latin.bundle ~/Library/Keyboard\ Layouts/British_Latin.bundle
+ln -s ~/.britishcustomkeyboard/British\ (Latin).bundle ~/Library/Keyboard\ Layouts/British\ (Latin).bundle
